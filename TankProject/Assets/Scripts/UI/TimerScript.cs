eb3f2466791c0fb8429b6dc3502b4f658ef86f4f
khoivu98 <khoivu98@yahoo.com.vn>
2019-10-29 03:28:24 -0400

    Added scripts

---
+++ TankProject/Assets/Scripts/UI/TimerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TimerScript : NetworkBehaviour {
+
+    [SyncVar]
+    public float time = 5f;
+
+    public Text InfoText;
+
+    void Start()
+    {
+        InfoText = GameObject.Find("Timer").GetComponent<Text>();
+    }
+    
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        InfoText.text = "Time: " + ((int)time).ToString();
+        if (time <= 0)
+            BackToLobby();
+	}
+
+    void BackToLobby()
+    {
+        FindObjectOfType<NetworkLobbyManager>().ServerReturnToLobby();
+    }
+}
